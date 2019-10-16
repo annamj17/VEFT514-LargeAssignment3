7e401ac683a0c82e9f03ad9de328e0672c6bfa3a
Ice3vil <annamj17@ru.is>
2019-10-16 18:10:55 +0000

    putting Errors in for getAllPlayers

--- resolvers/playerResolver.js
+++ resolvers/playerResolver.js
@@ -1,11 +1,11 @@
 const playerService = require('../services/playerService');
-const { InternalServerError } = require('../errors');
+const { NotFoundError } = require('../errors');
 
 module.exports = {
     Query: {
         allPlayers: async () => {
             const players = await playerService.getAllPlayers();
-            if (players === null) throw new InternalServerError;
+            if (players === null) throw new NotFoundError;
             return players.map(player => {
                 return {
                     ...playerService.returnPlayer(player)
