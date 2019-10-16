0bd2518212eaedd0f799909e52ec46a6d756c4f9
Ice3vil <annamj17@ru.is>
2019-10-16 23:32:23 +0000

    working on pickupGame

--- services/playerService.js
+++ services/playerService.js
@@ -1,16 +1,17 @@
 const { Player } = require('../data/db.js');
+const { InternalServerError } = require('../errors');
 
 const playerService = () => {
     const getAllPlayers = () => {
         const allPlayers = Player.find({}, function (error) {
-            if (error) error(new InternalServerError)
+            if (error) { error(new InternalServerError); }
         })
         return allPlayers;
     }
 
     const getPlayerById = (id) => {
         const player = Player.findById(id, function (error) {
-            if (error) error(new InternalServerError)
+            if (error) { error(new InternalServerError); }
         })
         return player;
     }
