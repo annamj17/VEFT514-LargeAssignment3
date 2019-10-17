a779760bfe04368e0ed5d55274ce8cb9e85d014b
Arnar <arnara17@ru.is>
2019-10-17 16:07:46 +0000

    pickupgames renamed to games

--- data/db.js
+++ data/db.js
@@ -3,11 +3,12 @@ const playerSchema = require('../schema/schemas/player');
 const pickupGameSchema = require('../schema/schemas/pickupGame');
 const basketballFieldSchema = require('../schema/schemas/basketballField');
 
-const connection = mongoose.createConnection('mongodb+srv://Anna:Abc1234@cluster0-hkoga.mongodb.net/hoop_dreams', { useNewUrlParser: true });
+const connection = mongoose.createConnection('mongodb+srv://Anna:Abc1234@cluster0-hkoga.mongodb.net/hoop_dreams', {useNewUrlParser: true});
+
 
 module.exports = {
-    Player: connection.model('Player', playerSchema),
-    PickupGame: connection.model('PickupGame', pickupGameSchema),
+    Player: connection.model('Players', playerSchema),
+    PickupGame: connection.model('games', pickupGameSchema),
     BasketballField: connection.model('BasketballField', basketballFieldSchema),
 };
 
